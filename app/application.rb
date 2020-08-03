f1042f606656900b6c071b357ec1661fb3e15fb9
afteralec <github email address>
2020-08-03 08:22:01 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,8 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -9,6 +11,20 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
+      end
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write 'Your cart is empty'
+      else
+        @@cart.each { |item| resp.write "#{item}\n" }
+      end
+    elsif req.path.match(/add/)
+      item = req.params['item']
+      if @@items.include?(item)
+        @@cart << item
+        resp.write("added #{item}")
+      else
+        resp.write("We don't have that item")
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
